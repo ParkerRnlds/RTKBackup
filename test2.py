b0e42a9bffec387d5ddd1810c583bfaee9f9bb64
Darius <dariussmedley@gmail.com>
2018-04-20 12:49:30 -0400

    Fixed merging issues

--- test2.py
+++ test2.py
@@ -1,19 +1,6 @@
 import serial
 import sys
 import time
-<<<<<<< HEAD
-while True:
-    serialcmd = chr(0xAA) + chr(0x0A) + chr(0x0A) + chr(0x7F)##
-    print("this shit IS working1")
-    s = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
-    print("this shit is not working2")
-    time.sleep(2)
-    print("this shit is not working3")
-    s.write(bytes(serialcmd, 'latin-1'))##  )    # motor 0 full speed reverse
-    print(serialcmd)
-    
-##s.write( chr(0xAA) )##+ chr(0x09) + chr(0x0A) + chr(0x00) )      # motor 0 stop
-=======
 def sendCommand(cmd1, cmd2):
     cmd = (chr(0xAA) + chr(0x0A) + chr(cmd1) + chr(cmd2)).encode()
     command = cmd.split(b'\xc2', 1)
@@ -26,5 +13,4 @@ time.sleep(5)
 sendCommand(0x08, 0x00) #motor 0 stop
 sendCommand(0x0E, 0x00) #motor 1 stop
 
->>>>>>> origin/master
 s.close()
